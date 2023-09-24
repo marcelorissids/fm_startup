d9afd878712b208abfd774bcd04d6e2aa4afa755
Marcelo Rissi <marcelorissi2@gmail.com>
2023-09-24 12:50:03 -0300

    demografico update

--- pages/3_Demográfico.py
+++ pages/3_Demográfico.py
@@ -8,7 +8,7 @@ from PIL import Image
 #from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
 
-dados_demograficos = pd.read_csv('/home/marcelo/repos_cds/kawa/data_treated/estados.csv')
+dados_demograficos = pd.read_csv('/data/estados.csv')
 
 
 def make_sidebar(df):
